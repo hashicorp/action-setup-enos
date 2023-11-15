21c1d427d8450d43af0073431660181bacd80e19
Ryan Cragun <me@ryan.ec>
2023-11-15 14:16:34 -0700

    release enos 0.0.24
    Signed-off-by: Ryan Cragun <me@ryan.ec>

--- README.md
+++ README.md
@@ -39,7 +39,7 @@ steps:
      github-token:
        ${{ secrets.GITHUB_TOKEN }}
      version:
-       0.0.23
+       0.0.24
 - name: Check Enos version
   run: enos version
 ```
--- dist/index.js
+++ dist/index.js
@@ -79,7 +79,7 @@ const githubRelease = __nccwpck_require__(3098);
 const executableName = 'enos';
 const gitHubRepositoryOwner = 'hashicorp';
 const gitHubRepositoryRepo = 'enos';
-const latestVersion = '0.0.23';
+const latestVersion = '0.0.24';
 
 async function downloadReleaseAsset(client, releaseAsset, directory) {
   try {
