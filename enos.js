f28779d831c228fa9f320d87ab0be8b2cb5dd864
Ryan Cragun <me@ryan.ec>
2023-12-01 13:02:27 -0700

    release enos 0.0.26 (#43)
    Signed-off-by: Ryan Cragun <me@ryan.ec>

--- enos.js
+++ enos.js
@@ -10,7 +10,7 @@ const githubRelease = require('./github-release');
 const executableName = 'enos';
 const gitHubRepositoryOwner = 'hashicorp';
 const gitHubRepositoryRepo = 'enos';
-const latestVersion = '0.0.24';
+const latestVersion = '0.0.26';
 
 async function downloadReleaseAsset(client, releaseAsset, directory) {
   try {
